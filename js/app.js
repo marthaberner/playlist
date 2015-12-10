9c9ade20c272b96dde7a66b8f2ebe46522a3d3eb
marthaberner <marthaberner.mb@gmail.com>
2015-12-10 16:06:25 -0700

    a solution

--- js/app.js
+++ js/app.js
@@ -1,30 +1,43 @@
 $(function () {
+  var albumTracks;
+
+  $('#clear').click(function () {
+    $('#my-tracks').html("").hide();
+  })
+
+  $('.submit').click(function () {
+    var userTracks = $('#my-tracks').html();
+    $.ajax({
+    method: "POST",
+    url: " https://lit-fortress-6467.herokuapp.com/post",
+    data: userTracks
+    }).done(function( msg ) {
+      console.log( "Data Saved: " + msg );
+    });
+  })
+
+  $('#my-tracks').hide();
   $( "button[name='choose-tracks']" ).click(function () {
     window.location.href = "tracks.html";
   })
 
   $(document).on("click", '.single-track', function () {
-    $('#my-tracks').append(this);
+    $('#my-tracks').show();
+    var albumId = $(this).attr('id');
+    var artist = getArtist(albumId);
+    $('#my-tracks').append('<p>'+ artist.artist + ': ' + artist.title + '</p>')
   })
 
   $.ajax({
     url: "https://lit-fortress-6467.herokuapp.com/object ",
     context: document.body
   }).done(function(results) {
+    albumTracks = results.results;
     var shuffledTracks = shuffleTracks(results.results);
-    console.log(shuffledTracks);
     showTracks(shuffledTracks, ".results" );
     showTracks(results.results, ".tracks");
-    // append album images to our container
-  });
 
-  $.ajax({
-  method: "POST",
-  url: " https://lit-fortress-6467.herokuapp.com/post",
-  }).done(function( msg ) {
-    console.log( "Data Saved: " + msg );
   });
-
 
   function shuffleTracks(array) {
     var results = [];
@@ -38,7 +51,18 @@ $(function () {
   function showTracks(array, element) {
     var gallery = $(element);
     array.forEach(function (track) {
-      gallery.append('<img class="single-track" src=images/' + track.cover_art + ' id=track-'+ track.id + '></img>');
+      gallery.append('<img class="single-track" src=images/' + track.cover_art + ' id='+ track.id + '></img>');
     })
+  }
+
+  function getArtist(albumId) {
+    var result = {}
+    for (var i = 0; i < albumTracks.length; i++) {
+      if(albumTracks[i].id == albumId){
+        result.artist = albumTracks[i].artist;
+        result.title = albumTracks[i].title;
+      }
+    }
+    return result;
   }
 })
